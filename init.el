68149237a735845229363dafbad1670a06e84312
Kyle <coder@kyleisom.net>
2015-09-14 22:33:41 -0700

    Might as well.

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
+(require 'cl)
 
 (let ((startup-time (current-time)))
   (setq inhibit-startup-echo-area-message
